fbfea92c7c733e98397dddaf3ac56b1e9bbe331c
Solene-Issartel <issartel.solene@gmail.com>
2021-11-17 16:32:15 +0100

    [ADD] sql create tables script

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS geolocations (
     geolocation_id serial PRIMARY KEY NOT NULL,
-    latitude double NOT NULL,
-    longitude double NOT NULL,
     geolocation_date timestamp WITHOUT TIME ZONE,
     user_id varchar(36) NOT NULL
 );
